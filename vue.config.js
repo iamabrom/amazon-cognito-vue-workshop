31c6657233df869bd3bd3d844b4551f5668f9c1e
EC2 Default User <ec2-user@ip-172-31-58-212.us-west-2.compute.internal>
2022-06-09 12:59:59 +0000

    initial commit after the fork

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,23 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+const webpack = require("webpack");
+
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ],
   },
   devServer: {
-    disableHostCheck: true
+    allowedHosts: 'all',
+    https: true,
+    proxy: {
+      '^/authn': {
+        target: 'http://localhost:8081',
+      }
+    }
   }
 };

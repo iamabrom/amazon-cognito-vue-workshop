66e81f29eb2cfe82dcc4ab23d4ade56836ea89bc
EC2 Default User <ec2-user@ip-172-31-58-212.us-west-2.compute.internal>
2022-06-09 13:11:23 +0000

    initial commit after the fork

--- src/config/cognito.js
+++ src/config/cognito.js
@@ -8,10 +8,10 @@
   replace with your UserPoolId and ClientId values here
 */
 export const POOL_DATA = {
-  UserPoolId: "us-west-2_7udo4YHd7",
-  ClientId: "53slmrlbl401h7820lj4joknth",
-  IdentityPoolId: "us-west-2:bdc8d3b2-2104-4555-bf6c-bc04051f5efb",
-  Region: "us-west-2",
-  IdentityPoolAuthProvider: "cognito-idp.us-west-2.amazonaws.com/us-west-2_7udo4YHd7",
-  ServiceEndpoint: "https://uaop74zaq5.execute-api.us-west-2.amazonaws.com/prod"
+  UserPoolId: "UserPoolId",
+  ClientId: "AppClientId",
+  IdentityPoolId: "IdentityPoolId",
+  Region: "region",
+  IdentityPoolAuthProvider: "AuthenticationProvider",
+  ServiceEndpoint: "ServiceEndpoint"
 };
